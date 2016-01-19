32d3452a7cfd16174f801ead49bd5f3256774e36
katsuramn <katsuramanakana@gmail.com>
2016-01-19 15:46:45 +0800

    design dutuu

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160119064916) do
+ActiveRecord::Schema.define(version: 20160119053957) do
 
   create_table "pages", force: :cascade do |t|
     t.string   "name"
     t.text     "content"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
-    t.string   "avatar_file_name"
-    t.string   "avatar_content_type"
-    t.integer  "avatar_file_size"
-    t.datetime "avatar_updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
