7f10794e55ca3168c01b5ab34d5d9305599066a5
katsuramn <katsuramanakana@gmail.com>
2016-01-25 10:56:19 +0800

    show zaswar

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,4 @@
 class Page < ActiveRecord::Base
-	has_attached_file :avatar, styles: { medium: "500x500#", thumb: "400x350#" }, default_url: "/images/:style/missing.png" 
+	has_attached_file :avatar, styles: { medium: "500x500>", thumb: "400x350#" }, default_url: "/images/:style/missing.png" 
 	validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
 end
