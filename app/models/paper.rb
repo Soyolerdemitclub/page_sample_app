c8eab68eccec3ccef5e4979ff416c36cf55db7b5
katsuramn <katsuramanakana@gmail.com>
2016-01-28 17:24:59 +0800

    Zaswaruud

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,5 @@
 class Paper < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :image, styles: { medium: "500x500>", thumb: "350x250#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
   extend FriendlyId
   friendly_id :name, use: :slugged
