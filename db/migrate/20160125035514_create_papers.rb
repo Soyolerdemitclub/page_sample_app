331cda54ef86a96382c9c021e0b1145fddd4484b
CaDiL <kkhoschuluu@gmail.com>
2016-02-07 12:34:32 +0800

    Paper link error fixed, remove slug on controller, migrate, model

--- db/migrate/20160125035514_create_papers.rb
+++ db/migrate/20160125035514_create_papers.rb
@@ -3,10 +3,7 @@ class CreatePapers < ActiveRecord::Migration
     create_table :papers do |t|
       t.string :name
       t.text :content
-      t.string :slug
 
       t.timestamps null: false
     end
-    add_index :papers, :slug, unique: true
-  end
 end
